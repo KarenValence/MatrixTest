40088133ce4912d44f95d78441e4581cb6a3b2bd
KarenValence <karen_odessa@hotmail.com>
2023-07-16 13:28:08 +0200

    first commit

---
+++ Src/main/java/MatrixTest.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @Test
+    void isSquareMatrix() {
+    }
+
+    @Test
+    void getAverageCountArray() {
+    }
+}
---
+++ Src/main/java/ProgramTest.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showMatrix() {
+    }
+}
